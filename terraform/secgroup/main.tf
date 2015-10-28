af3947c66ab83dc76026258dc978ba3e4361897c
Andrii Loshkovskyi <mail@loshkovskyi.net>
2015-10-28 20:09:52 +0200

    Open new ports in security group with Terraform

--- terraform/secgroup/main.tf
+++ terraform/secgroup/main.tf
@@ -1,34 +1,52 @@
 variable cluster_name { }
 
 resource "openstack_compute_secgroup_v2" "cluster" {
-    name = "${ var.cluster_name }"
-    description = "Security Group for ${ var.cluster_name }"
+    name          = "${ var.cluster_name }"
+    description   = "Security Group for ${ var.cluster_name }"
     rule {
-      from_port = -1
-      to_port = -1
+      from_port   = -1
+      to_port     = -1
       ip_protocol = "icmp"
-      self = true
+      self        = true
     }
     rule {
-      from_port = 22
-      to_port = 22
+      from_port   = 22
+      to_port     = 22
       ip_protocol = "tcp"
-      cidr = "0.0.0.0/0"
+      cidr        = "0.0.0.0/0"
     }
     rule {
-      from_port = 53
-      to_port = 53
+      from_port   = 53
+      to_port     = 53
       ip_protocol = "udp"
-      cidr = "0.0.0.0/0"
+      cidr        = "0.0.0.0/0"
     }
     rule {
-      from_port = 53
-      to_port = 53
+      from_port   = 53
+      to_port     = 53
       ip_protocol = "tcp"
-      cidr = "0.0.0.0/0"
+      cidr        = "0.0.0.0/0"
+    }
+    rule {
+      from_port   = 8443
+      to_port     = 8443
+      ip_protocol = "tcp"
+      cidr        = "0.0.0.0/0"
+    }
+    rule {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      cidr        = "0.0.0.0/0"
+    }
+    rule {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      cidr        = "0.0.0.0/0"
     }
 }
 
 output "cluster_name" {
-  value = "${ openstack_compute_secgroup_v2.cluster.name }"
+  value           = "${ openstack_compute_secgroup_v2.cluster.name }"
 }
